7e5315cbcf3b5958fc299b17f81c26b86e8cc1ca
Giulia Pelosi <giulia.pelosi@dgsspa.com>
2024-02-01 09:52:59 +0100

    init commit

---
+++ src/main/java/com/dgsspa/marvel/JpaMarvelExampleApplication.java
@@ -0,0 +1,13 @@
+package com.dgsspa.marvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMarvelExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaMarvelExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgsspa/marvel/JpaMarvelExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsspa.marvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMarvelExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
