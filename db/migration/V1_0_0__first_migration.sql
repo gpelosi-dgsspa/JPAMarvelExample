694bc800d5bb7bceb55b73d85c62097b87fa51cf
mattia_96 <mattia.sardellini.guest@agenziariscossione.gov>
2024-02-01 12:36:01 +0100

    Aggiunti file configurazione per FlyWay più aggiunta file di Script sql per l'aggiunta delle tabelle personaggi, film, abilita

--- db/migration/V1_0_0__first_migration.sql
+++ db/migration/V1_0_0__first_migration.sql
@@ -1,28 +1,3 @@
-CREATE TABLE IF NOT EXISTS `personaggi` (
-
-    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Nome` varchar(20),
-    `Razza` varchar(50),
-    `Eta` timestamp,
-    `DataNascita` timestamp
-
-    )ENGINE=InnoDB DEFAULT CHARSET=UTF8;
-
-CREATE TABLE IF NOT EXISTS `film` (
-
-    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Nome` varchar(20),
-    `Regista` varchar(50),
-    `DataUscita` timestamp
-
-    )ENGINE=InnoDB DEFAULT CHARSET=UTF8;
-
-CREATE TABLE IF NOT EXISTS `abilita` (
-
-    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Abilita` varchar(20),
-    `Potenza` varchar(50),
-    `EroeID` int,
-    FOREIGN KEY (EroeID) REFERENCES personaggi(id)
-
-    )ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+CREATE TABLE personaggi (id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,Nome varchar(20),Razza varchar(50),Eta timestamp NULL,DataNascita timestamp NULL) ;
+CREATE TABLE film (id int NOT NULL AUTO_INCREMENT PRIMARY KEY,Nome varchar(20),Regista varchar(50),DataUscita timestamp NULL);
+CREATE TABLE abilita (id int NOT NULL AUTO_INCREMENT PRIMARY KEY,Abilita varchar(20),Potenza varchar(50),EroeID int,FOREIGN KEY (EroeID) REFERENCES personaggi(id));
