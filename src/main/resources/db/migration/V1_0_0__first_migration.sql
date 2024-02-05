1168e2024a7a65f94254dec35e94bbb5ea956daf
mattia_96 <mattia.sardellini.guest@agenziariscossione.gov>
2024-02-05 14:53:27 +0100

    Modifica script SQL - Modifica file migration

--- src/main/resources/db/migration/V1_0_0__first_migration.sql
+++ src/main/resources/db/migration/V1_0_0__first_migration.sql
@@ -1,4 +1,4 @@
 CREATE SCHEMA IF NOT EXISTS javamarvelexample;
 CREATE TABLE personaggi (id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,Nome varchar(200),Razza varchar(50),Eta integer NULL,DataNascita timestamp NULL);
-CREATE TABLE film (id int NOT NULL AUTO_INCREMENT PRIMARY KEY,Nome varchar(200),Regista varchar(50),DataUscita timestamp NULL);
-CREATE TABLE abilita (id int NOT NULL AUTO_INCREMENT PRIMARY KEY,Abilita varchar(200),Potenza varchar(50),EroeID int,FOREIGN KEY (EroeID) REFERENCES personaggi(id));
+CREATE TABLE film (id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,Nome varchar(200),Regista varchar(50),DataUscita timestamp NULL);
+CREATE TABLE abilita (id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,Abilita varchar(200),Potenza varchar(50),EroeID int,FOREIGN KEY (EroeID) REFERENCES personaggi(id));
